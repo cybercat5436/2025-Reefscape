6841bfc4db7243046179307c2132f35902ca3ac1
Cyber Cats <cybercatsprogramming@gmail.com>
2025-02-28 18:35:13 -0500

    added motion magic to climber inorder to move them to the starting position that mechanical wants in parallel to the first path of both autons

--- src/main/java/frc/robot/subsystems/Climber2.java
+++ src/main/java/frc/robot/subsystems/Climber2.java
@@ -4,12 +4,17 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -22,15 +27,18 @@ public class Climber2 extends SubsystemBase {
     private final DutyCycleOut rightOut = new DutyCycleOut(0);
     private double rightEncoderValue;
     private double leftEncoderValue;
-    private double ClimberEncoderLimitUp = 0;
-    private double ClimberEncoderLimitDown = -5;
+    private double climberEncoderLimitUp = 0;
+    private double climberEncoderLimitDown = -5;
+    private double climberAutonStartPosition = -2;
+    private final MotionMagicVoltage m_mmReq = new MotionMagicVoltage(0);
+
 
     /** Creates a new Climber. */
   public Climber2() {
     var leftConfiguration = new TalonFXConfiguration();
     var rightConfiguration = new TalonFXConfiguration();
   
-    leftConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+    leftConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     rightConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
     leftConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
@@ -41,6 +49,36 @@ public class Climber2 extends SubsystemBase {
 
     leftClimber.setPosition(0);
     rightClimber.setPosition(0);
+
+
+
+    MotionMagicConfigs mml = leftConfiguration.MotionMagic;
+    mml.withMotionMagicCruiseVelocity(20) // 5 (mechanism) rotations per second cruise
+      .withMotionMagicAcceleration(40) // Take approximately 0.5 seconds to reach max vel
+      // Take approximately 0.1 seconds to reach max accel 
+      .withMotionMagicJerk((200));
+
+    Slot0Configs leftSlot0 = leftConfiguration.Slot0;
+    leftSlot0.kS = 0.25; // Add 0.25 V output to overcome static friction
+    leftSlot0.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    leftSlot0.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+    leftSlot0.kP = 60; // A position error of 0.2 rotations results in 12 V output
+    leftSlot0.kI = 0; // No output for integrated error
+    leftSlot0.kD = 0.5; // A velocity error of 1 rps results in 0.5 V output
+
+    MotionMagicConfigs mmr = rightConfiguration.MotionMagic;
+    mmr.withMotionMagicCruiseVelocity(20) // 5 (mechanism) rotations per second cruise
+      .withMotionMagicAcceleration(40) // Take approximately 0.5 seconds to reach max vel
+      // Take approximately 0.1 seconds to reach max accel 
+      .withMotionMagicJerk((200));
+
+    Slot0Configs rightSlot0 = rightConfiguration.Slot0;
+    rightSlot0.kS = 0.25; // Add 0.25 V output to overcome static friction
+    rightSlot0.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+    rightSlot0.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+    rightSlot0.kP = 60; // A position error of 0.2 rotations results in 12 V output
+    rightSlot0.kI = 0; // No output for integrated error
+    rightSlot0.kD = 0.5; // A velocity error of 1 rps results in 0.5 V output
   }
 /**
  * This method makes the robot climb and score LOTS of points
@@ -48,10 +86,10 @@ public class Climber2 extends SubsystemBase {
   * 
   */
  public void leftClimb(double speed) {
-  if((leftEncoderValue >= ClimberEncoderLimitUp) && (speed > 0)){
+  if((leftEncoderValue >= climberEncoderLimitUp) && (speed > 0)){
     speed = 0.0;
   }
-  if((leftEncoderValue <= ClimberEncoderLimitDown) && (speed < 0)){
+  if((leftEncoderValue <= climberEncoderLimitDown) && (speed < 0)){
     speed = 0.0;
   }
   leftClimber.setControl(leftOut.withOutput(speed));
@@ -60,14 +98,19 @@ public class Climber2 extends SubsystemBase {
 
 
  public void rightClimb(double speed) {
-  if((rightEncoderValue >= ClimberEncoderLimitUp) && (speed > 0)){
+  if((rightEncoderValue >= climberEncoderLimitUp) && (speed > 0)){
     speed = 0.0;
   }
-  if((rightEncoderValue <= ClimberEncoderLimitDown) && (speed < 0)){
+  if((rightEncoderValue <= climberEncoderLimitDown) && (speed < 0)){
     speed = 0.0;
   }
   rightClimber.setControl(rightOut.withOutput(speed));
   SmartDashboard.putNumber("ClimberRightSpeed", rightClimber.get());
+ }
+
+ public void climberAutonStartPosition(){
+  rightClimber.setControl(m_mmReq.withPosition(climberAutonStartPosition).withSlot(0));
+  leftClimber.setControl(m_mmReq.withPosition(climberAutonStartPosition).withSlot(0));
  }
  
 
