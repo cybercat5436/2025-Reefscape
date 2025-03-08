cdf38f885b7072b696c7b064a126139a6c7993e7
Manjushri <manjushri.ug@gmail.com>
2025-03-08 12:51:43 -0500

    Remove reset Pose from teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -72,7 +72,7 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    m_robotContainer.resetSimulationStartPose();
+    // m_robotContainer.resetSimulationStartPose();
   }
 
   @Override
